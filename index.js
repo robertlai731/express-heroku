9bd0493a13f3cbab06087c0c74528330cf011fcc
robertlai731 <diamondlai88@yahoo.com.tw>
2017-09-11 20:00:12 +0800

    update

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+app.get('/hi', function (req, res) {
+  res.send('Hi World!')
+})
+
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`)
 })
