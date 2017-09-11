24899fc8348e39d72a2a2e580cc29b144d5b30e1
robertlai731 <diamondlai88@yahoo.com.tw>
2017-09-11 20:27:38 +0800

    update  geoapi

--- index.js
+++ index.js
@@ -8,14 +8,15 @@ app.get('/', function (req, res) {
 })
 
 app.get('/hi', function (req, res) {
-  res.send('Hi World!')
+  
   let address = 'National Taiwan University'
   let url = `https://maps.googleapis.com/maps/api/geocode/json?address=${address}`
   request(url, 
       function (error, response, body) {
-    console.log('error:', error);
+    console.log('error:', error)
     console.log('statusCode:', response.statusCode);
-    console.log('body:', body);
+    console.log('body:', body)
+    res.send(body)
   });
 })
 
